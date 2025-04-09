bb205cd1bcacd112678dabb22d421d32d4594726
AbdulMoeez1278 <abdulmoeez1278@gmail.com>
2025-04-10 01:50:43 +0500

    Start a new project

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+console.log("Website is in progress...");
+
+// const currloc = location.href;
+// const menuItem = document.querySelectorAll("a");
+// const menuLen = menuItem.length;
+// for (let i = 0; i < menuLen; i++) {
+//   menuItem[i].classList.remove("active");
+//   if (menuItem[i].href === currloc) {
+//     menuItem[i].className = "nav-link active";
+//   }
+// }
