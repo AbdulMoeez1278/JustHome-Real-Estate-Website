ec1c02b7c3d63f831eaae60e3bb9f677d315a9ff
AbdulMoeez1278 <abdulmoeez1278@gmail.com>
2025-04-16 00:26:40 +0500

    Create the sidebar and its logic also correct the code.

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,49 @@
+// sidebar logic
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("sideNavbar");
+const cross = document.getElementById("cross");
+
+hamburger.addEventListener("click", () => {
+  sidebar.style.display = "block";
+  hamburger.style.visibility = "hidden";
+  cross.style.display = "block";
+});
+
+cross.addEventListener("click", () => {
+  sidebar.style.display = "none";
+  hamburger.style.visibility = "visible";
+  cross.style.display = "none";
+});
+
+//scroll to the top - logic
+// const scrollToTopButton = document.getElementById("scroll-arrow");
+
+// scrollToTopButton.addEventListener("click", function () {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// });
+
 // swiper js library
 const swiper = new Swiper(".swiper", {
   // Optional parameters
-  slidesPerView: 3,
-  spaceBetween: 10,
+  // slidesPerView: 3,
+  // spaceBetween: 10,
   direction: "vertical",
   loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  speed: 500,
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+  // speed: 500,
 
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-    type: "bullets",
+    //   clickable: true,
+    //   dynamicBullets: true,
+    //   type: "bullets",
   },
 
   // Navigation arrows
